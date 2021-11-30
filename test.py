85d7397c012d5c2e99379085b851ea46d7f5a04f
Kiwy <nathan.davouse@utt.fr>
2021-11-30 17:11:39 +0100

    class + def init

--- test.py
+++ test.py
@@ -3,6 +3,35 @@
 Created on Tue Nov  9 13:55:33 2021
 @author: natha
 """
+T = 200 #définir l'horizon global
 
-ListA = [12,15,19,16,33,64,87,67,65]
-print("ListA : ",ListA)
+class variable:
+    #définir tous les éléments qu'on retrouve a chaque fois
+    def __init__(self):
+        self.D=[]
+        self.t=[]
+        self.R=[]
+        self.xR=[]
+        self.xM=[]
+        self.yR=[]
+        self.yM=[]
+        self.GammaM=[]
+        self.GammaT=[]
+
+"""        
+def Demand:
+Faire une fonction qui va remplir chaque demande
+"""
+
+classeur = [variable]*(T+1)
+
+def net_requi(t):
+    somme = 0
+    for i in range (t):
+        somme += (classeur[i].D-classeur[i].R)
+    somme -= classeur[0].yR    
+
+
+        
+
+
